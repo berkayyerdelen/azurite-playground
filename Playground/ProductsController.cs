894dd9b95564658a196c6317e1203c952f2d20d8
MSI\berka <berkayyerdelen@gmail.com>
2024-05-26 00:21:54 +0300

    initialize project

---
+++ Playground/ProductsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Playground;
+[ApiController]
+[Route("v1/api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Products()
+    {
+        return Ok("product");
+    }
+  
+}
